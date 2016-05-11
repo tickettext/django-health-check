c1469adc9bd828951e10cfaa4abcaa7f0865ff48
James Bailey <james.bailey@ticket-text.com>
2016-05-11 18:11:05 +0100

    Put transaction.atomic() around creation of TestModel instance
    See https://docs.djangoproject.com/en/1.9/topics/db/transactions/#controlling-transactions-explicitly. We need to make sure the query that raised the
    DatabaseError has been rolled back before handling the exception;
    otherwise we'll get a TransactionManagementError next time we try to do a
    db read or write. We were seeing this when trying to get the result of the
    celery health check after the db health check had failed with a
    DatabaseError, because the celery check tries to read the task result from
    the database.

--- health_check_db/plugin_health_check.py
+++ health_check_db/plugin_health_check.py
@@ -1,6 +1,6 @@
 from health_check.backends.base import BaseHealthCheckBackend, ServiceUnavailable, ServiceReturnedUnexpectedResult
 from health_check_db.models import TestModel
-from django.db import DatabaseError, IntegrityError
+from django.db import DatabaseError, IntegrityError, transaction
 from health_check.plugins import plugin_dir
 
 class DjangoDatabaseBackend(BaseHealthCheckBackend):
@@ -10,10 +10,11 @@ class DjangoDatabaseBackend(BaseHealthCheckBackend):
 
     def check_status(self):
         try:
-            obj = TestModel.objects.create(title="test")
-            obj.title = "newtest"
-            obj.save()
-            obj.delete()
+            with transaction.atomic():
+                obj = TestModel.objects.create(title="test")
+                obj.title = "newtest"
+                obj.save()
+                obj.delete()
             return True
         except IntegrityError:
             raise ServiceReturnedUnexpectedResult("Integrity Error")
